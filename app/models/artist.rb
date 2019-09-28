5443b10343b177275b0622c59e4409b0012f51af
Austin Asbury <acasbury25@aol.com>
2019-09-28 01:26:06 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,6 +14,14 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs
+    count = 0
+    genre_array = []
+    self.songs.each do |song|
+      if !genre_array.include?(song.genre)
+        count += 1
+        genre_array < song.genre
+      end
+    end
+    count
   end
 end
