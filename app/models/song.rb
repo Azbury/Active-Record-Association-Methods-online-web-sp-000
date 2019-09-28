9bc4fba1d4af1992a30e349a3db1de4e70b28ea7
Austin Asbury <acasbury25@aol.com>
2019-09-28 01:54:30 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,7 +14,7 @@ class Song < ActiveRecord::Base
       self.artist = Artist.all.map{|artist| artist.name = "Drake"}.first
     else
       Artist.create(name: "Drake")
-      self.artist = Artist.where("artists.name = 'Drake'")
+      self.artist = Artist.all.map{|artist| artist.name = "Drake"}.first
     end
   end
 end
